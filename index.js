17a3fb53e96d376d9656bbe08e6ea7874df52e9e
Jeremy <jeremyVDLRC@gmail.com>
2020-07-18 12:55:18 +1000

    added name, user and desc inputs to array

--- index.js
+++ index.js
@@ -2,7 +2,46 @@ const fs = require("fs");
 const inquirer = require("inquirer");
 
 // array of questions for user
-const questions = [
+const questions = [{
+        type: "input",
+        name: "repoName",
+        message: "Enter the title of your application (Required)",
+        validateName: (name) => {
+            if (name) {
+                return true;
+            } else {
+                console.log("Enter the name of your app!");
+                return false;
+            }
+        }
+    },
+    {
+        type: "input",
+        name: "userName",
+        message: "Enter your GitHub username (Required)",
+        validateName: (username) => {
+            if (username) {
+                return true;
+            } else {
+                console.log("Enter your GitHub username!");
+                return false;
+            }
+        }
+    },
+
+    {
+        type: "input",
+        name: "description",
+        message: "Enter a description for your application",
+        validate: (descInput) => {
+            if (descInput) {
+                return true;
+            } else {
+                descInput = ""
+            }
+        }
+    },
+
 
 ];
 

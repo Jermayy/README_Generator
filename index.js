f5def63470c4542a39015e4ecdbb0dbbe1ff71de
Jeremy <jeremyVDLRC@gmail.com>
2020-07-18 20:53:41 +1000

    Added licence function

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
 const inquirer = require("inquirer");
+const { table } = require("console");
 
 // array of questions for user
 const questions = [{
@@ -129,13 +130,105 @@ const questions = [{
 
 ];
 
-// function to write README file
-function writeToFile(fileName, data) {}
 
 // function to initialize program
 function init() {
 
-}
+    const tableOfContent = "  \n ## Table of Contents:  \n[1. Description](#Description)  \n[2. Installation](#Installation)  \n[3. App Usage](#App-Usage)  \n[4. License Details](#License-Details)  \n[5. List of Contributors](#List-of-Contributors)  \n[6. Tests](#Tests)  \n[7. Questions](#Questions)  \n";
 
-// function call to initialize program
-init();
+    inquirer.prompt(questions).then((res) => {
+            title();
+
+            function title() {
+                fs.writeToFile("./dist/README.md",
+                    `# ${res.repoName} \r\n`,
+                    (err) => {
+                        if (err) {
+                            console.log(err);
+                            return;
+
+                        }
+                        console.log("Repo Name Added");
+                        licence();
+                    }
+                );
+            }
+
+            //generate licence Badge "GPL V3", "EPL 1.0", "MIT", "MPL 2.0"
+
+            function licence() {
+                if (res.licence === "GPL V3") {
+                    fs.appendFileSync(
+                        "./dist/README.md",
+                        `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`,
+                        (err) => {
+                            if (err) {
+                                console.log(err);
+                                return;
+                            }
+                            console.log("Licence: GPL V3");
+                            table();
+                        }
+                    );
+                } else if (res.licence === "EPL 1.0") {
+                    fs.appendFileSync(
+                        "./dist/README.md",
+                        `![License](https://img.shields.io/badge/License-EPL%201.0-red.svg)](https://opensource.org/licenses/EPL-1.0)`,
+                        (err) => {
+                            if (err) {
+                                console.log(err);
+                                return;
+                            }
+                            console.log("Licence: EPL 1.0");
+                            table();
+                        }
+                    );
+                } else if (res.licence === "MIT") {
+                    fs.appendFileSync(
+                        "./dist/README.md",
+                        `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`,
+                        (err) => {
+                            if (err) {
+                                console.log(err);
+                                return;
+                            }
+                            console.log("Licence: MIT");
+                            table();
+                        }
+                    );
+                } else if (res.licence === "MPL 2.0") {
+                    fs.appendFileSync(
+                        "./dist/README.md",
+                        `[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)`,
+                        (err) => {
+                            if (err) {
+                                console.log(err);
+                                return;
+                            }
+                            console.log("Licence: MPL 2.0");
+                            table();
+                        }
+                    );
+                }
+
+
+
+
+
+
+
+
+
+
+
+
+            })
+
+
+
+
+
+    }
+
+    // function call to initialize program
+    init();

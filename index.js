a15a86ed0a86641334d618a26ca6d7628373bf1f
Jeremy <jeremyVDLRC@gmail.com>
2020-07-18 12:32:44 +1000

    add fs and inquirer requires

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
+const fs = require("fs");
+const inquirer = require("inquirer");
+
 // array of questions for user
 const questions = [
 
 ];
 
 // function to write README file
-function writeToFile(fileName, data) {
-}
+function writeToFile(fileName, data) {}
 
 // function to initialize program
 function init() {
